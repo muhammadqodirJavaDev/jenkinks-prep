08d2d02f4594f87bba19babe9a5fd301c8cb60d3
Muhammadqodir Berdiyev <m.berdiev@uzum.com>
2024-07-07 17:25:13 +0500

    set up

---
+++ src/main/java/com/example/prepjenkins/PrepJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.prepjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepJenkinsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrepJenkinsApplication.class, args);
+  }
+
+}
